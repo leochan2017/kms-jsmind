68e9fb30bed9d3162029a4f1d8af322c65716f1a
Leo <leochan2017@gmail.com>
2018-05-15 16:04:27 +0800

    format code

--- src/kmsjsmap.js
+++ src/kmsjsmap.js
@@ -5,27 +5,28 @@ var _noop = function() {};
 
 // 河蟹IE8 时 没有Object.keys方法
 if (!Object.keys) {
-  Object.keys = (function () {
+  Object.keys = (function() {
     'use strict';
     var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
+      hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+      dontEnums = [
+        'toString',
+        'toLocaleString',
+        'valueOf',
+        'hasOwnProperty',
+        'isPrototypeOf',
+        'propertyIsEnumerable',
+        'constructor'
+      ],
+      dontEnumsLength = dontEnums.length;
 
-    return function (obj) {
+    return function(obj) {
       if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
         throw new TypeError('Object.keys called on non-object');
       }
 
-      var result = [], prop, i;
+      var result = [],
+        prop, i;
 
       for (prop in obj) {
         if (hasOwnProperty.call(obj, prop)) {
